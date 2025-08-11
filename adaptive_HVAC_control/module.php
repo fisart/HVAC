360c5fae8c0217b981eae4adb36fdb68330539e4
fisart <1234fisart@gmail.com>
2025-08-11 13:20:59 +0200

    apply changes

--- adaptive_HVAC_control/module.php
+++ adaptive_HVAC_control/module.php
@@ -93,6 +93,7 @@ class adaptive_HVAC_control extends IPSModule
 
     public function ApplyChanges()
     {
+        $this->log(0, 'ApplyChanges_START', []); 
         parent::ApplyChanges();
 
         // ---- one-time migration (no recursion) ----
@@ -136,6 +137,7 @@ class adaptive_HVAC_control extends IPSModule
         }
 
         $this->log(2, 'apply_changes', ['interval_ms' => $intervalMs]);
+        $this->log(0, 'ApplyChanges_END', []); 
     }
 
     // -------------------- Public APIs for Orchestrator/UI --------------------
